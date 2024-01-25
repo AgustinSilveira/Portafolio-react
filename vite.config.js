8d4e374e99e78baef1fcad5e188ca6b900a750c5
Agustín <agustinsilveira2021@gmail.com>
2024-01-25 04:18:19 -0300

    arreglos del build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import { ViteAliases } from 'vite-aliases';
+import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
-
+  plugins: [
+    react(),
+    VitePWA(),
+    ViteAliases({
+      'src': '/src',
+    }),
+    WindiCSS(),
+  ],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
+});
