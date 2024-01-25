5bc771eb50c7aa721295c106488f4583d6a9dd29
Agustín <agustinsilveira2021@gmail.com>
2024-01-25 13:54:21 -0300

    error dist

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import { ViteAliases } from 'vite-aliases';
-import WindiCSS from 'vite-plugin-windicss';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA(),
-    ViteAliases({
-      'src': '/src',
-    }),
-    WindiCSS(),
-  ],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-  },
-});
+  plugins: [react()],
+})
+
